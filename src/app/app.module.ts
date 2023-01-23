0026dd1535917a82bad22faffa30c07c902f551b
Arialita <alejandro_aguilar_1996@hotmail.com>
2023-01-22 22:38:58 -0300

    terminado la parte responsiva, a resolver: foto de perfil e imagen de fondo, trabajar en los servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { TrabajoComponent } from './components/sections/trabajo/trabajo.component';
+import { EducacionComponent } from './components/sections/educacion/educacion.component';
+import { ProyectoComponent } from './components/sections/proyecto/proyecto.component';
+import { HabilidadComponent } from './components/sections/habilidad/habilidad.component';
+import { ContactoComponent } from './components/home/contacto/contacto.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    TrabajoComponent,
+    EducacionComponent,
+    ProyectoComponent,
+    HabilidadComponent,
+    ContactoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
